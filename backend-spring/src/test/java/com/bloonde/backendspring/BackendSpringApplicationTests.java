650fe629318ef342179e17cd4c53b372e7d1555b
Daniel Velázquez <danvelcab@gmail.com>
2019-08-07 16:11:43 +0200

    .first commit spring

---
+++ backend-spring/src/test/java/com/bloonde/backendspring/BackendSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bloonde.backendspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
